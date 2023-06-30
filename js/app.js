d0bf243cbfee185e6c0f7fb9867bd836cf4c5f75
Rony Pablo <ronyalexis0@gmail.com>
2023-06-29 19:10:12 -0700

    cd

--- js/app.js
+++ js/app.js
@@ -1,18 +1,28 @@
 
 
 window.onload = function(){
+    let emailState = false;
+
     let emailModal = document.getElementsByClassName('email-modal')[0];
     console.log(emailModal);
 
     let closeModal = document.getElementsByClassName('email-modal__close-btn')[0];
 
+    let emailInput = document.getElementsByClassName('email-modal__input')[0];
+
     closeModal.addEventListener('click', () => {
         emailModal.classList.remove('email-modal--visible');
     });
     
-    document.addEventListener('mouseleave', () => {
-        emailModal.classList.add('email-modal--visible');
-        console.log('mouse left')
+    let showModal = () => {
+        if(emailState == false){
+            emailModal.classList.add('email-modal--visible');
+            emailState = true;
+        }
+    }
+
+    document.body.addEventListener('mouseleave', () => {
+        showModal();
     });
 
     console.log(document)
