09080befecb020a7371cf9977d16d422ab5268ec
Rony Pablo <ronyalexis0@gmail.com>
2023-07-02 16:12:07 -0700

    cleaner javascript

--- js/app.js
+++ js/app.js
@@ -6,12 +6,40 @@ window.onload = function(){
     let emailModal = document.getElementsByClassName('email-modal')[0];
     console.log(emailModal);
 
-    let closeModal = document.getElementsByClassName('email-modal__close-btn')[0];
+    let closeButton = document.getElementsByClassName('email-modal__close-btn')[0];
 
     let emailInput = document.getElementsByClassName('email-modal__input')[0];
 
-    closeModal.addEventListener('click', () => {
+    let emailButton = document.getElementsByClassName('email-modal__button')[0];
+
+    function emailIsValid(email){
+        return /\S+@\S+\.\S+/.test(email)
+    }
+
+    let closeModal = () => {
         emailModal.classList.remove('email-modal--visible');
+    }
+
+    let addErrors = () => {
+        document.getElementsByClassName('email-modal__form-group')[0].classList.add('email-modal__form-group--error');
+        document.getElementsByClassName('email-modal__error-message')[0].classList.add('email-modal__error-message--active');
+    }
+
+    let removeErrors = () => {
+        document.getElementsByClassName('email-modal__form-group')[0].classList.remove('email-modal__form-group--error');
+        document.getElementsByClassName('email-modal__error-message')[0].classList.remove('email-modal__error-message--active');
+    }
+
+    emailButton.addEventListener('click', () => {
+        if(emailIsValid(emailInput.value)){
+            console.log(emailInput.value)
+        } else {
+            addErrors();
+        }
+    })
+
+    closeButton.addEventListener('click', () => {
+        closeModal();
     });
     
     let showModal = () => {
